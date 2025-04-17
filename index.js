c2f71cf98a812c44a36c70ecb9480d1365f7610f
Your NameChristian <christianghermann@gmail.com>
2025-04-17 15:38:52 -0500

    complete

---
+++ index.js
@@ -0,0 +1,16 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = 90 / 2 - 5;
+
+let code3 = 37 + (20 % 3);
+
+let message = "The vault has been secured. The combination is:";
+console.log(message);
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA);
+
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB);
